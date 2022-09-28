1c67f443bf5646bec25ba256f76e5c830d0396d1
Riquelme Milhomem <91623489+Rick-Farias@users.noreply.github.com>
2022-09-28 18:48:56 -0300

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+const btn_menu = document.getElementById("btn-menu");
+
+btn_menu.addEventListener("click", () => {
+  const menu_2 = document.getElementById("nav-list-2");
+  menu_2.classList.toggle("menu-on-off");
+console.log("teste")
+});
