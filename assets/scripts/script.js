e40ac64828d1336730fffcb5879d37e71b6c9daa
Riquelme Milhomem <91623489+Rick-Farias@users.noreply.github.com>
2023-03-01 16:56:08 -0300

    Add files via upload

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,44 @@
 const btn_menu = document.getElementById("btn-menu");
+const home = document.getElementById("homeId");
+const produtos = document.getElementById("produtosId");
+const local = document.getElementById("localId");
+
+home.addEventListener("click",()=>{
+  const menu_2 = document.getElementById("nav-list-2");
+  menu_2.classList.toggle("menu-on-off");
+})
+
+produtos.addEventListener("click",()=>{
+  const menu_2 = document.getElementById("nav-list-2");
+  menu_2.classList.toggle("menu-on-off");
+})
+
+local.addEventListener("click",()=>{
+  const menu_2 = document.getElementById("nav-list-2");
+  menu_2.classList.toggle("menu-on-off");
+})
+
 
 btn_menu.addEventListener("click", () => {
   const menu_2 = document.getElementById("nav-list-2");
   menu_2.classList.toggle("menu-on-off");
-console.log("teste")
+
 });
+
+// Declarar variaveis
+
+let contador = 1;
+document.getElementById("slide1").checked = true;
+
+  setInterval(function(){
+    nextImage();
+  } ,5000)
+
+  function nextImage(){
+    contador++;
+    if(contador>5){
+      contador = 1;
+    }
+    
+    let radio = document.getElementById("slide"+contador).checked = true 
+  }
